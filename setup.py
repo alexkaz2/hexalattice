7f4bd223d1d6d24e47b0097b2998f513fb168303
AlexKaz <alex.kazakov@mail.huji.ac.il>
2020-10-09 03:39:57 +0300

    Restructuring to fit pypi packaging format

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup
+import setuptools
 import pathlib
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # This call to setup() does all the work
-setup(
-    name="hexalattice",
+setuptools.setup(
+    name="hexalattice-alexkaz",
     version="1.0.0",
     description="Compute and plot hexagonal grids",
-    long_description=README,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alexkaz2/hexalattice/wiki",
     author="Alex Kazakov",
@@ -23,6 +24,14 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["hexalattice"],
-    install_requires=["numpy", "matplotlib"],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+    install_requires=[
+        "numpy",
+        "matplotlib >= 3.1",
+    ],
+    setup_requires=[
+        "numpy",
+        "matplotlib >= 3.1",
+    ],
 )
